b04552d9744efb8c7ebc33ed8c6e2ab720be40c5
Konrad Kędziorek <konradk453@gmail.com>
2022-10-06 17:42:40 +0200

    Reinit

---
+++ src/main/java/pl/kedziorek/mpkoperator/MpkOperatorApplication.java
@@ -0,0 +1,13 @@
+package pl.kedziorek.mpkoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpkOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpkOperatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kedziorek/mpkoperator/MpkOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kedziorek.mpkoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpkOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
