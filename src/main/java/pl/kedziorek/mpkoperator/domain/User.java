b284d23b52ca6176208b1a827c46356683a99bdb
Konrad Kędziorek <konradk453@gmail.com>
2022-10-22 17:42:16 +0200

    Added role and user tables and models. Configuration liquibase and added new logs in dbchangelogs

---
+++ src/main/java/pl/kedziorek/mpkoperator/domain/User.java
@@ -0,0 +1,29 @@
+package pl.kedziorek.mpkoperator.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> role;
+}
